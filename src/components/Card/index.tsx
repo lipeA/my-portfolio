7f2194c570f5e0bd2858db6332bb1e183b03ea0c
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2025-08-11 14:59:46 -0300

    feat: add new project in the portifilio

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,14 +9,24 @@ interface CardProps {
 export default function Card({ image, title, text }: CardProps) {
   return (
     <>
-      <a href="#" className="hover:scale-105 transition-transform duration-300">
+      <a
+        href="https://github.com/lipeA/react-native-participantes"
+        target="_blank"
+        className="hover:scale-105 transition-transform duration-300"
+      >
         <div className="bg-bgPrimary w-[330px] h-[280px] rounded-md p-3">
-          <div className="w-full h-[150px] bg-gray-300 rounded-t-md ">
-            <Image src={image} alt="Imagem do projeto" />
+          <div className="w-full h-[150px] bg-gray-300 rounded-t-md overflow-hidden ">
+            <Image
+              src={image}
+              alt="Imagem do projeto"
+              className="image-card-project"
+            />
           </div>
-          <div className="mt-5 ml-1 flex flex-col items-start">
+          <div className="mt-5 ml-1 flex flex-col items-start ">
             <h3 className="text-tercery font-bold text-[16px]">{title}</h3>
-            <p className="text-[14px] text-secondary ">{text}</p>
+            <p className="text-[14px] text-secondary text-ellipsis whitespace-nowrap overflow-hidden truncate max-w-72">
+              {text}
+            </p>
           </div>
         </div>
       </a>
--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -1,5 +1,6 @@
 import Card from "../Card";
 import Image from "../../../public/img/image 2.png";
+import Inhere from "../../../public/img/inhere.jpg";
 export default function ProjectsSection() {
   return (
     <div className=" w-full flex flex-col justify-center items-center p-10 max-md:overflow-x-hidden  max-md:p-1 max-md:mt-10 max-md:pb-28">
@@ -11,11 +12,11 @@ export default function ProjectsSection() {
           Veja os projetos em destaques
         </h2>
       </div>
-      <div className="w-[1040px] h-[584px] justify-center items-center grid grid-cols-3 gap-4 mt-9  max-md:flex  max-md:overflow-x-scroll max-md:h-fit max-md:justify-start max-md:pl-2 max-md:w-full">
+      <div className="w-[1040px] h-[584px] justify-center items-center grid grid-cols-3 gap-4 mt-9  max-md:flex  max-md:flex-col max-md:h-fit max-md:justify-start max-md:pl-2 max-md:w-full">
         <Card
-          image={Image}
-          title="Projeto 01"
-          text="informações sobre o projeto "
+          image={Inhere}
+          title=" app em React Native para gerenciar participantes"
+          text="No processo, trabalhei com useState, FlatList, Alert e outros componentes do próprio React Native, buscando deixar a interface limpa e intuitiva"
         />
         <Card
           image={Image}
